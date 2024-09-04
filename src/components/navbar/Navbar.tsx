03109e8cf168b2a2e31dcb8adb63744889acb722
lucas_souza_ribeiro <lucas.srs.2022@gmail.com>
2024-09-04 11:40:07 -0300

    rotas navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Bell, SignOut, User } from '@phosphor-icons/react'
 import logo from "../../assets/logonobg.png"
 import "./navbar.css"
+import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
@@ -9,8 +10,12 @@ function Navbar() {
                 <img src={logo} alt="Logo" className="h-20" />
                 <h2 className=" font-montSerrat uppercase">farmacostore</h2>
             </div>
-            <div>
+            <div className='flex items-center font-semibold uppercase'>
                 <input className="rounded-md  w-64 h-8 p-3 uppercase placeholder" placeholder="pesquisar" />
+                <ul className='ml-2 *:mx-2'>
+                    <Link to="/cadastroCategoria" className='hover:text-indigo-900'>cadastrar categoria</Link>
+                    <Link to="/categorias" className='hover:text-indigo-900'>categorias</Link>
+                </ul>
             </div>
             <div className="flex">
                 <button className="mr-10"><Bell size={32} color="#78716C" weight="bold" /></button>
